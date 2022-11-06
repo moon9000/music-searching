18cf73f470ed298573c7301accb9de0c78b1a31e
Sabri <sabri.abdennader@wandi.fr>
2022-11-06 22:58:47 +0100

    feat(src/features/music-fetching/musicFetchingSlice): use https for artistRelationsApi, add songApi

--- src/features/music-fetching/musicFetchingSlice.ts
+++ src/features/music-fetching/musicFetchingSlice.ts
@@ -18,7 +18,7 @@ export const artistApi = createApi({
 export const artistRelationsApi = createApi({
   reducerPath: "artistRelationsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://musicbrainz.org/ws/2/artist`,
+    baseUrl: `https://musicbrainz.org/ws/2/artist`,
   }),
   endpoints: (builder) => ({
     getArtistByMbid: builder.query<ArtistMbidData, string>({
@@ -27,7 +27,21 @@ export const artistRelationsApi = createApi({
   }),
 });
 
+export const songApi = createApi({
+  reducerPath: "songApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://ws.audioscrobbler.com/2.0`,
+  }),
+  endpoints: (builder) => ({
+    getSongByName: builder.query<ArtistData, string>({
+      query: (name) =>
+        `?method=track.getinfo&track=${name}&api_key=38be8fec8a756d6754565408178ceb6e&format=json`,
+    }),
+  }),
+});
+
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
 export const { useGetArtistByNameQuery } = artistApi;
 export const { useGetArtistByMbidQuery } = artistRelationsApi;
+export const { useGetSongByNameQuery } = songApi;
