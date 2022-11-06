28cef2b78f7700dec9e9b80970d498bd4de54806
Sabri <sabri.abdennader@wandi.fr>
2022-11-06 21:09:01 +0100

    feat(app/store): add artistApi and artistRelationsApi reducer paths and middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,24 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import {
+  artistApi,
+  artistRelationsApi,
+} from "../features/music-fetching/musicFetchingSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    // Add the generated reducer as a specific top-level slice
+    [artistApi.reducerPath]: artistApi.reducer,
+    [artistRelationsApi.reducerPath]: artistRelationsApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      artistApi.middleware,
+      artistRelationsApi.middleware
+    ),
 });
 
 export type AppDispatch = typeof store.dispatch;
